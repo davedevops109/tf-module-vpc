910437db88e17b1886dff5b9aa16b8d3b396075b
EPAPRA <dave.devops109@gmail.com>
2023-08-07 14:29:11 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -21,4 +21,5 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = "vpc-0724d763db4589c34"
   vpc_id        = aws_vpc.main.id
+  auto_accept   = true
 }
