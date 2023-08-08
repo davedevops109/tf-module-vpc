3440fbf48b4e6f0c0c31f6b4d56475c1b5b0ec56
EPAPRA <dave.devops109@gmail.com>
2023-08-08 19:58:42 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -70,6 +70,12 @@ resource "aws_route_table" "public" {
   )
 }
 
+resource "aws_route_table_association" "public-rt-assoc" {
+  count = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
 //create EC2
 /*data "aws_ami" "centos8" {
   most_recent      = true
