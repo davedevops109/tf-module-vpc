e640310e18d831846db7afabddea576cb485b036
EPAPRA <dave.devops109@gmail.com>
2023-08-11 20:21:24 +0530

    multibranch commit

--- subnets/main.tf
+++ subnets/main.tf
@@ -23,7 +23,7 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "association" {
-  count = length(aws_subnet.main)
+  count          = length(aws_subnet.main)
   subnet_id      = aws_subnet.main.*.id[count.index]
   route_table_id = aws_route_table.route_table.id
 }
@@ -32,5 +32,5 @@ resource "aws_route" "internet_gw_route" {
   count                       = var.internet_gw ? 0 : 1
   route_table_id              = aws_route_table.route_table.id
   destination_cidr_block      = "0.0.0.0/0"
-  gateway_id                  = var.internet_gw
+  gateway_id                  = var.gateway_id
 }
