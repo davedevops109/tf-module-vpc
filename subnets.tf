da8af34a5f37a929cc9dab1162b6eb06922be3d3
EPAPRA <dave.devops109@gmail.com>
2023-08-14 06:34:11 +0530

    multibranch commit

--- subnets.tf
+++ subnets.tf
@@ -17,17 +17,17 @@
 #  gateway_id          = aws_internet_gateway.gw.id
 #}
 
-module "public_subnets" {
+module "private_subnets" {
   source = "./subnets"
     default_vpc_id      = var.default_vpc_id
     env                 = var.env
     availability_zone   = var.availability_zone
 
-    for_each            = var.public_subnets
+    for_each            = var.private_subnets
     cidr_block          = each.value.cidr_block
     name                = each.value.name
     internet_gw         = lookup(each.value, "internet_gw", false)
-  #  nat_gw              = lookup(each.value, "nat_gw", false)
+    nat_gw              = lookup(each.value, "nat_gw", false)
 
     vpc_id              = aws_vpc.main.id
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
