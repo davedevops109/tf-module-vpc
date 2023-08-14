562cecfdb9df5246bd711cef9f2c58df8246d464
EPAPRA <dave.devops109@gmail.com>
2023-08-14 06:27:26 +0530

    multibranch commit

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,7 @@ output "public_subnet_ids" {
 }
 
 output "one_subnet_id" {
-  value = lookup(lookup(module.public_subnets, "public", {}), "subnet_ids", [])[0]
+  value = lookup(lookup(module.public_subnets, "public", null), "subnet_ids", null)[0]
 #  value = module.public_subnets["public"]["subnet_ids"][0]
 
 }
